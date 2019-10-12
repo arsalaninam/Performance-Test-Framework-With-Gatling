d6b913e3d7e3741a66aa09c050255388473d99c5
Arsalan Inam <m.arsalaninam@gmail.com>
2019-10-12 21:03:41 +0500

    Added File Util to write the response data in a file

--- src/test/scala/util/FileUtil.scala
+++ src/test/scala/util/FileUtil.scala
@@ -1,5 +1,20 @@
 package util
 
+import java.io.{BufferedWriter, File, FileWriter}
+
 object FileUtil {
+
+  def writeUserData(lines: String): Unit = {
+    var file = new File(Constants.SERVICE_JSON_BASE_PATH)
+    if (!file.exists()) {
+      file.mkdirs()
+      file = new File(Constants.SERVICE_JSON_BASE_PATH + Constants.SERVICE_JSON_FILENAME)
+      file.createNewFile()
+    }
+    val bw = new BufferedWriter(new FileWriter(file))
+    bw.write(lines)
+    bw.close()
+  }
+
 
 }
