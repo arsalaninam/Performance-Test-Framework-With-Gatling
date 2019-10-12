b539671119f434d596d65b8cbc3a1f135c510535
Arsalan Inam <m.arsalaninam@gmail.com>
2019-10-12 21:21:58 +0500

    Comments added

--- src/test/scala/simulation/CreateUserSimulation.scala
+++ src/test/scala/simulation/CreateUserSimulation.scala
@@ -10,18 +10,34 @@ import util.{PropertiesReader, Constants, FileUtil}
 
 import scala.util.Random
 
-class CreateUserSimulation extends Simulation{
+class CreateUserSimulation extends Simulation {
 
+  /*
+  Setting up Http Protocols (BaseUrl , Headers etc.)
+   */
   private val httpProtocol: HttpProtocolBuilder = http
     .baseUrl(PropertiesReader.getProperty(Constants.BASE_URL))
     .acceptHeader(Constants.JSON_CONTENT_TYPE)
 
+  // User Request Name
   val userCreateRequest: String = "User Create Request"
+
+  // Create User API Response JSON declaration
   var userCreateResponseJson = new JSONArray
+
+  /*
+  generating random names for Create User Request Payload
+   */
   val nameFeeder = Iterator.continually(
     Map("name" -> Random.alphanumeric.filter(_.isLetter).take(5).mkString),
   )
 
+  /*
+  Creating a Scenario for Create User API.
+  It will execute post request to provided API Url,
+  check the response status and response body,
+  and save the id from response.
+   */
   val createUserScenario: ScenarioBuilder = scenario(Constants.CREATE_USER_SCENARIO_NAME)
     .feed(nameFeeder)
     .exec(http(userCreateRequest)
@@ -37,12 +53,18 @@ class CreateUserSimulation extends Simulation{
     session
   }).pause(4)
 
+  /*
+  In setUp(), we will inject the desired load to our API
+   */
   setUp(
     createUserScenario.inject(
-      constantUsersPerSec(Constants.USER_REQUESTS_PER_SECOND) during(3)
+      constantUsersPerSec(Constants.USER_REQUESTS_PER_SECOND) during (3)
     )
   ).protocols(httpProtocol)
 
+  /*
+  Id from each response will be saved in a new file in JSON format
+   */
   after {
     FileUtil.writeUserData(userCreateResponseJson.toString)
   }
