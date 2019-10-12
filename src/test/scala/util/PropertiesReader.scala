b976ce11b8e17958d4c063e8455f2ba66838d44b
Arsalan Inam <m.arsalaninam@gmail.com>
2019-10-12 21:03:52 +0500

    Added Properties Reader

--- src/test/scala/util/PropertiesReader.scala
+++ src/test/scala/util/PropertiesReader.scala
@@ -1,5 +1,22 @@
 package util
 
+import com.typesafe.config.ConfigFactory
+
 object PropertiesReader {
+
+  def getProperty(name: String): String = {
+
+    var env = System.getProperty("environment")
+
+    if (env != null) {
+      env = "-" + env
+    }
+    else {
+      env = ""
+    }
+
+    val config = ConfigFactory.load("application" + env + ".properties")
+    config.getString(name)
+  }
 
 }
