7d87a1d8e99c14518e0f893728b608a94e65709e
Arsalan Inam <m.arsalaninam@gmail.com>
2019-10-31 14:04:46 +0500

    Revert "Add Test Case — Added Test Cases to validate Response Status Code, Content Type and Body"
    This reverts commit 252efc4cddb5d15f3ea9f52117c9208f6238a88c.

--- src/test/scala/util/PropertiesReader.scala
+++ src/test/scala/util/PropertiesReader.scala
@@ -4,9 +4,6 @@ import com.typesafe.config.ConfigFactory
 
 object PropertiesReader {
 
-  /*
-  It will read the property from application.properties file
-   */
   def getProperty(name: String): String = {
 
     var env = System.getProperty("environment")
